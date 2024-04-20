ea17e5cc261fd6103b8a2fd77bc495142cb7e396
dheerajgill25 <77674668+dheerajgill25@users.noreply.github.com>
2024-04-20 21:40:53 +0530

    first commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, Injector} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent  {
+  title = 'frontend';
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+}
